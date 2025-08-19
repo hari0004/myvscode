1d2ce6431072823293ad653c1382e0cb713f150c
hari0004 <harikaiplakal@gmail.com>
2025-08-19 11:53:45 +0530

    Add only main.tf

---
+++ main.tf
@@ -0,0 +1,95 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
+# 1. VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# 2. Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# 3. Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# 4. Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# 5. Route Table Association
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 6. Security Group
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-sg"
+  }
+}
+
+# 7. EC2 Instance
+resource "aws_instance" "DevOps_ubuntu" {
+  ami                         = "ami-044415bb13eee2391" # Ubuntu 22.04 (London)
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.devops_sg.id]
+  associate_public_ip_address = true
+  #key_name                    = "Docker" # replace or set variable below
+
+  tags = {
+    Name = "DevOps-ubuntu"
+  }
+}
